d09419baab6d62e737a777035b36e51879ec9400
fejes99 <david.fejes@gmail.com>
2024-09-07 22:40:49 +0200

    Added Swagger

--- api/src/main/java/se/david/api/composite/order/service/OrderCompositeService.java
+++ api/src/main/java/se/david/api/composite/order/service/OrderCompositeService.java
@@ -1,5 +1,11 @@
 package se.david.api.composite.order.service;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,26 +14,97 @@ import se.david.api.composite.order.dto.OrderAggregateDto;
 
 import java.util.List;
 
+@Tag(name = "Order Composite Service", description = "REST API for composite order information.")
 public interface OrderCompositeService {
   @GetMapping(
     value = "/order-composite",
     produces = "application/json")
+  @Operation(
+    summary = "Get all composite orders",
+    description = "Retrieves a list of composite order information.",
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved the list of composite orders",
+        content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(implementation = OrderAggregateDto.class, type = "array")
+        )
+      )
+    }
+  )
   List<OrderAggregateDto> getCompositeOrders();
 
   @GetMapping(
     value = "/order-composite/user/{userId}",
     produces = "application/json")
+  @Operation(
+    summary = "Get composite orders by user ID",
+    description = "Retrieves a list of composite orders specific to the user based on user ID.",
+    parameters = @Parameter(name = "userId", description = "ID of the user to fetch composite orders for", required = true, schema = @Schema(type = "integer")),
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved the list of composite orders for the specified user",
+        content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(implementation = OrderAggregateDto.class, type = "array")
+        )
+      ),
+      @ApiResponse(
+        responseCode = "404",
+        description = "User with the specified ID not found"
+      )
+    }
+  )
   List<OrderAggregateDto> getCompositeOrdersByUser(@PathVariable int userId);
 
   @GetMapping(
     value = "/order-composite/{orderId}",
     produces = "application/json")
+  @Operation(
+    summary = "Get composite order by order ID",
+    description = "Retrieves detailed composite order information based on the provided order ID.",
+    parameters = @Parameter(name = "orderId", description = "ID of the order to retrieve", required = true, schema = @Schema(type = "integer")),
+    responses = {
+      @ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved the composite order for the specified ID",
+        content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(implementation = OrderAggregateDto.class)
+        )
+      ),
+      @ApiResponse(
+        responseCode = "404",
+        description = "Order with the specified ID not found"
+      )
+    }
+  )
   OrderAggregateDto getCompositeOrder(@PathVariable int orderId);
+
 
   @PostMapping(
     value = "/order-composite",
     consumes = "application/json",
     produces = "application/json")
+  @Operation(
+    summary = "Create a new composite order",
+    description = "Creates a new composite order based on the provided order details.",
+    responses = {
+      @ApiResponse(
+        responseCode = "201",
+        description = "Successfully created the composite order",
+        content = @Content(
+          mediaType = "application/json",
+          schema = @Schema(implementation = OrderAggregateDto.class)
+        )
+      ),
+      @ApiResponse(
+        responseCode = "400",
+        description = "Invalid input data"
+      )
+    }
+  )
   OrderAggregateDto createCompositeOrder(@RequestBody OrderAggregateDto orderAggregateDto);
-
 }
--- microservices/order-composite-service/src/main/java/se/david/microservices/composite/order/OrderCompositeServiceApplication.java
+++ microservices/order-composite-service/src/main/java/se/david/microservices/composite/order/OrderCompositeServiceApplication.java
@@ -1,5 +1,8 @@
 package se.david.microservices.composite.order;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +12,24 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("se.david")
 public class OrderCompositeServiceApplication {
+  @Value("${api.common.version}")
+  private String apiVersion;
+
+  @Value("${api.common.title}")
+  private String apiTitle;
+
+  @Value("${api.common.description}")
+  private String apiDescription;
+
+  @Bean
+  public OpenAPI getOpenApiDocumentation() {
+    return new OpenAPI()
+      .info(new Info()
+        .title(apiTitle)
+        .description(apiDescription)
+        .version(apiVersion));
+  }
+
   @Bean
   RestTemplate restTemplate() {
     return new RestTemplate();
